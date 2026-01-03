f6419b77c363a75ecd958827e16de9e8dab339f9
Raj <86660451+Raj3019@users.noreply.github.com>
2026-01-03 14:09:56 +0530

    frontend port change

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.use(cors({
-   origin:'http://localhost:3001',
+   origin:'http://localhost:8080',
    credentials: true
 }))
 
